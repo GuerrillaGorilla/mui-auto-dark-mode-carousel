b0ebf4ce598b705c6a6144ce67d71867412cdf01
guerrillagorilla <65793435+FathymTester@users.noreply.github.com>
2022-12-18 14:45:49 -0500

    Small test change

--- src/App.js
+++ src/App.js
@@ -200,7 +200,23 @@ function App() {
           </AccordionSummary>
           <AccordionDetails>
             <Typography>
-              
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+              malesuada lacus ex, sit amet blandit leo lobortis eget.
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel2a-content"
+            id="panel2a-header"
+          >
+            <Typography>Accordion 3</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+              malesuada lacus ex, sit amet blandit leo lobortis eget.
             </Typography>
           </AccordionDetails>
         </Accordion>
